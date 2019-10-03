37bd9b27954792f79c81b648c296415e4218048f
karlos6 <Carlos.Keller.M@gmail.com>
2019-10-03 09:08:13 -0500

    sad

---
+++ HotelRouting/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
+
+
+const routes: Routes = [  
+  {    
+    path: "home",
+    component: HomeComponent     
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/home"
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ HotelRouting/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
